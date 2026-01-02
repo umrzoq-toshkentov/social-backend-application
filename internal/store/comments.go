4aa575c50f5f1fd296faa13f16f6df584d928267
umrzoq-toshkentov <ssdumrzoq@gmail.com>
2026-01-02 12:06:04 +0500

    feat: added comments store

---
+++ internal/store/comments.go
@@ -0,0 +1,48 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Comment struct {
+	ID        int64  `json:"id" validate:"required"`
+	PostID    int64  `json:"post_id" validate:"required"`
+	UserID    int64  `json:"user_id" validate:"required"`
+	Content   string `json:"content" validate:"required"`
+	CreatedAt string `json:"created_at" validate:"required"`
+	User      User   `json:"user"`
+}
+
+type CommentStore struct {
+	db *sql.DB
+}
+
+func (s *CommentStore) GetByPostId(ctx context.Context, postId int64) ([]Comment, error) {
+	query := `
+		SELECT c.id, c.post_id, c.user_id, c.content, c.created_at, users.username, users.id FROM comments c
+		JOIN users on users.id = c.user_id WHERE c.post_id = $1
+		ORDER BY c.created_at DESC
+	`
+	rows, err := s.db.QueryContext(ctx, query, postId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	comments := []Comment{}
+	for rows.Next() {
+		var comment Comment
+		comment.User = User{}
+		if err := rows.Scan(&comment.ID, &comment.PostID, &comment.UserID, &comment.Content, &comment.CreatedAt, &comment.User.Username, &comment.User.ID); err != nil {
+			return nil, err
+		}
+		comments = append(comments, comment)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return comments, nil
+}
